62c4e3ad292b3da9396efcfa4585abbe98650747
AdamEdmondson <adamjedmondson@gmail.com>
2026-06-09 00:36:47 +0100

    Fixed formatting
    Fixed Formatting on Time Round, Round 2 & Time 2 so text and other assets scale and position properly. The font has also been updated to look cleaner.

--- src/c/fergusSource.c
+++ src/c/fergusSource.c
@@ -6,6 +6,7 @@
 // Enjoy this little cat on your wrist!!
 // - Adam Edmondson :)
 
+
 #include <pebble.h>
 
 static Window* s_main_window;
@@ -14,7 +15,6 @@ static TextLayer* s_date_layer;
 static Layer* s_battery_layer;
 static BitmapLayer* s_cat_layer;
 static GBitmap* s_cat_bitmap;
-
 static GFont s_time_font;
 static GFont s_date_font;
 static int s_battery_level;
@@ -41,14 +41,13 @@ static void battery_update_proc(Layer* layer, GContext* ctx)
 {
     GRect bounds = layer_get_bounds(layer);
 
-    // Calculate width dynamically based on the layer's assigned bounds
     int width = (s_battery_level * bounds.size.w) / 100;
 
-    // Draw the background
+    // Background
     graphics_context_set_fill_color(ctx, GColorWhite);
     graphics_fill_rect(ctx, bounds, 0, GCornerNone);
 
-    // Draw the bar
+    // Bar
     graphics_context_set_fill_color(ctx, GColorBlack);
     graphics_fill_rect(ctx, GRect(0, 0, width, bounds.size.h), 0, GCornerNone);
 }
@@ -75,39 +74,70 @@ static void bluetooth_callback(bool connected)
 
 static void main_window_load(Window* window)
 {
-    // Get information about the Window
     Layer* window_layer = window_get_root_layer(window);
     GRect bounds = layer_get_bounds(window_layer);
 
-    // Calculate dynamic centering variables
     int16_t w = bounds.size.w;
-    
-    // Offset centers the layout on taller screens.
-    int16_t y_offset = (bounds.size.h - 168) / 2;
+    int16_t h = bounds.size.h; 
 
-    // Create GFont
-    s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_digital_60));
-    s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_digital_24));
+    // Create Font
+    #if PBL_DISPLAY_HEIGHT >= 228
+        s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_digital_72));
+        s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_digital_36));
+    #else
+        s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_digital_60));
+        s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_digital_24));
+    #endif
 
-    // Create GBitmap & BitmapLayer
     s_cat_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CAT);
     
+    // Layout variables
+    int cat_y, cat_h, date_y, date_h, time_y, time_h, bat_w, bat_x, bat_y;
+
+    #if defined(PBL_ROUND)
+        cat_y = (h * 5) / 100;
+        cat_h = (h * 40) / 100; 
+        
+        date_y = (h * 40) / 100;
+        date_h = (h * 15) / 100; 
+        
+        time_y = (h * 55) / 100;
+        time_h = (h * 42) / 100; 
+        
+        bat_w = (w * 80) / 100;  
+        bat_x = (w - bat_w) / 2;
+        bat_y = (h * 60) / 100; 
+    #else
+        cat_y = -(h * 6) / 100;
+        cat_h = (h * 50) / 100;
+        
+        date_y = (h * 38) / 100;
+        date_h = (h * 18) / 100;
+        
+        time_y = (h * 55) / 100;
+        time_h = (h * 42) / 100;
+        
+        bat_w = (w * 80) / 100;
+        bat_x = (w - bat_w) / 2;
+        bat_y = (h * 58) / 100;
+    #endif
+
     // Cat Layer
-    s_cat_layer = bitmap_layer_create(GRect(0, -5 + y_offset, w, 80));
+    s_cat_layer = bitmap_layer_create(GRect(0, cat_y, w, cat_h));
     bitmap_layer_set_bitmap(s_cat_layer, s_cat_bitmap);
     bitmap_layer_set_alignment(s_cat_layer, GAlignCenter);
     layer_add_child(window_layer, bitmap_layer_get_layer(s_cat_layer));
 
-    // Date TextLayer
-    s_date_layer = text_layer_create(GRect(0, 65 + y_offset, w, 30));
+    // Date Layer
+    s_date_layer = text_layer_create(GRect(0, date_y, w, date_h));
     text_layer_set_background_color(s_date_layer, GColorClear);
     text_layer_set_text_color(s_date_layer, GColorBlack);
     text_layer_set_font(s_date_layer, s_date_font);
     text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
     layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 
-    // Time TextLayer
-    s_time_layer = text_layer_create(GRect(0, 90 + y_offset, w, 70));
+    // Time Layer
+    s_time_layer = text_layer_create(GRect(0, time_y, w, time_h));
     text_layer_set_background_color(s_time_layer, GColorClear);
     text_layer_set_text_color(s_time_layer, GColorBlack);
     text_layer_set_text(s_time_layer, "00:00");
@@ -116,23 +146,21 @@ static void main_window_load(Window* window)
     layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 
     // Battery Layer
-    int bat_w = 114;
-    int bat_x = (w - bat_w) / 2;
-    s_battery_layer = layer_create(GRect(bat_x, 98 + y_offset, bat_w, 2));
+    s_battery_layer = layer_create(GRect(bat_x, bat_y, bat_w, 2));
     layer_set_update_proc(s_battery_layer, battery_update_proc);
     layer_add_child(window_layer, s_battery_layer);
 }
 
 static void main_window_unload(Window* window)
 {
-    // Destroy elements
+    // Destroy
     text_layer_destroy(s_time_layer);
     text_layer_destroy(s_date_layer);
     bitmap_layer_destroy(s_cat_layer);
     gbitmap_destroy(s_cat_bitmap);
     layer_destroy(s_battery_layer);
 
-    // Unload GFonts
+    // Unload Fonts
     fonts_unload_custom_font(s_time_font);
     fonts_unload_custom_font(s_date_font);
 }
@@ -147,7 +175,7 @@ static void update_time(TimeUnits units_changed)
     static char s_buffer[8];
     strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
 
-    // Display this time on the TextLayer
+    // Display time
     text_layer_set_text(s_time_layer, s_buffer);
 
     if (units_changed & DAY_UNIT)
@@ -156,7 +184,7 @@ static void update_time(TimeUnits units_changed)
         static char s_date_buffer[16];
         strftime(s_date_buffer, sizeof(s_date_buffer), "%a %b %d", tick_time);
 
-        // Display the date
+        // Display Date
         text_layer_set_text(s_date_layer, s_date_buffer);
     }
 }
@@ -168,10 +196,7 @@ static void tick_handler(struct tm* tick_time, TimeUnits units_changed)
 
 static void init()
 {
-    // Create main Window element and assign to pointer
     s_main_window = window_create();
-
-    // Change the background color
     window_set_background_color(s_main_window, GColorWhite);
 
     // Set handlers to manage the elements inside the Window
@@ -187,10 +212,10 @@ static void init()
     tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
     battery_state_service_subscribe(battery_callback);
 
-    // Battery level is displayed from the start
+    // Displayed from the start
     battery_callback(battery_state_service_peek());
 
-    // Register for Bluetooth connection updates
+    // Register for Bluetooth updates
     connection_service_subscribe((ConnectionHandlers)
     {
         .pebble_app_connection_handler = bluetooth_callback
